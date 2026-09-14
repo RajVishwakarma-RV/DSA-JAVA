c1f73066e00da5fabce7049ad7601a514c23e085
Raj Vishwakarma <rajvishwakarma1784@gmail.com>
2026-09-14 23:25:00 +0530

    Two Sum code

---
+++ DSA/Array/TwoSum.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSum {
+    
+    // Non-static method (ya ise static bhi bana sakte ho: static int[] twoSum...)
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i]; 
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        
+       
+        TwoSum solver = new TwoSum();
+        int[] result = solver.twoSum(nums, 5);
+        
+       
+        System.out.println(Arrays.toString(result)); 
+    }
+}
--- OOPS Basics/Basic1.java
+++
@@ -1,8 +0,0 @@
-public class Basic1{
-    String r ="Hi";
-    String m ="I am Raj";
-    public static void main(String[] args) {
-        Basic1 a1 = new Basic1();
-        System.out.println(a1.r+" "+a1.m);
-    }
-}
